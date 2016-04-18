f4d06a9d2130edad95f8f23b4d5eb484f1393a46
Maciej Bożemój <maciejbozemoj@hotmail.com>
2016-04-18 23:27:02 +0200

    Version 1.02, Rdy for tests

--- PIFS/app/src/main/java/pl/wroc/pwr/pifs/LoginActivity.java
+++ PIFS/app/src/main/java/pl/wroc/pwr/pifs/LoginActivity.java
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -73,6 +74,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
--- PIFS/app/src/main/java/pl/wroc/pwr/pifs/services_details.java
+++ PIFS/app/src/main/java/pl/wroc/pwr/pifs/services_details.java
@@ -3,16 +3,18 @@ package pl.wroc.pwr.pifs;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -22,15 +24,16 @@ public class services_details extends Activity {
     TextView txtOrderID;
     TextView txtBrand;
     TextView txtModel;
-    TextView txtStatus;
-    TextView txtMore;
+    TextView txtcurStatus;
+    TextView txtDesc;
+    TextView txtDateFrom;
+    TextView txtDateTo;
+    TextView txtPrice;
 
     String id_order;
     String tokenID;
     // Progress Dialog
     private ProgressDialog pDialog;
-    // products JSONArray
-    JSONArray orders = null;
     // JSON parser class
     JSONParser jsonParser = new JSONParser();
 
@@ -42,6 +45,7 @@ public class services_details extends Activity {
     private static final String TAG_ID_ORDER = "Id";
     private static final String TAG_DEVICEMODEL = "DeviceModel";
     private static final String TAG_DEVICEBRAND = "DeviceBrand";
+    private static final String TAG_CURRENTSTATE = "CurrentState";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +53,10 @@ public class services_details extends Activity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         setContentView(R.layout.activity_services_details);
+
+        //TextView tx = (TextView)findViewById(R.id.text_desc);
+        //Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/ComfortaaThin.ttf");
+        //tx.setTypeface(custom_font);
 
         GlobalCatcher mApp = ((GlobalCatcher)getApplicationContext());
         tokenID = mApp.getSomeVariable2();
@@ -103,13 +111,15 @@ public class services_details extends Activity {
                     }
                     try {
                         // Getting Array of Products
-                        orders = json.getJSONArray(TAG_ORDERS);
+                        JSONArray orders = json.getJSONArray(TAG_ORDERS);
                         // looping through All Products
                         for (int i = 0; i < orders.length(); i++) {
                             JSONObject c = orders.getJSONObject(i);
                             String id = c.getString(TAG_ID_ORDER);
+                            JSONObject d = c.getJSONObject(TAG_CURRENTSTATE);
                             if (id.equals(id_order)) {
-                                Log.d("AllServices", "We've got this rec");
+                                Log.d("AllServices", "We've clicked this rec:");
+                                Log.d("AllServices", c.toString());
                                 // Storing each json item in variable
                                 String rec_id = c.getString(TAG_ID_ORDER);
                                 String rec_brand = c.getString(TAG_DEVICEBRAND);
@@ -121,13 +131,53 @@ public class services_details extends Activity {
                                 txtModel = (TextView) findViewById(R.id.text_model);
                                 txtModel.setText(rec_model);
 
+                                //String rec1_curStatus = d.getString("State");
+                                //String rec1_desc = d.getString("Description");
+                                //String rec1_price = d.getString("Price");
+                                try{
+                                    String rec1_curStatus = new String(d.getString("State").getBytes("ISO-8859-1"), "UTF-8");
+                                    txtcurStatus = (TextView) findViewById(R.id.text_status);
+                                    txtcurStatus.setText(rec1_curStatus);
+
+                                    String rec1_desc = new String(d.getString("Description").getBytes("ISO-8859-1"), "UTF-8");
+                                    txtDesc = (TextView) findViewById(R.id.text_desc);
+                                    txtDesc.setText(rec1_desc);
+
+                                    String rec1_price = new String(d.getString("Price").getBytes("ISO-8859-1"), "UTF-8");
+                                    txtPrice = (TextView) findViewById(R.id.text_price);
+                                    txtPrice.setText(rec1_price);
+                                } catch (Exception e){
+                                    Log.e("AllServices", e.getMessage());
+                                }
+
+
+                                String rec1_dateFrom = d.getString("DateFrom");
+                                String rec1_dateFrom2 = rec1_dateFrom.replaceAll("T", " ");
+                                String rec1_dateFrom3 = rec1_dateFrom2.split("\\.", 2)[0];
+
+                                txtDateFrom = (TextView) findViewById(R.id.text_Dfrom);
+                                txtDateFrom.setText(rec1_dateFrom3);
+
+                                String rec1_dateTo = d.getString("DateTo");
+                                String rec1_dateTo2 = rec1_dateTo.replaceAll("T", ",");
+                                String rec1_dateTo3 = rec1_dateTo2.split("\\.", 2)[0];
+                                txtDateTo = (TextView) findViewById(R.id.text_Dto);
+                                txtDateTo.setText(rec1_dateTo3);
+
+
+
+
+
+
+
+
+
+
                             }
-
-
-
                         }
-                    } catch (Exception e){
-                        Log.d("AllServices", "Catched error2");
+                    } catch (JSONException e){
+                        Log.e("AllServices", e.getMessage());
+                        e.printStackTrace();
                     }
                 }
             });
